a7bcec9f79cf3793f68a275cfb9564c3fe81aff7
Didinga Omodi <129117745+Didinga@users.noreply.github.com>
2024-02-13 11:28:32 +0100

    Create funny-names-generator.py

---
+++ funny-names-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the funny names generator app")
+name = input("What is your first name?\n")
+description = input("What is your typical trait? Write it in uppercase letters.\n")
+print("Your funny name is " + name + " "+ description)
